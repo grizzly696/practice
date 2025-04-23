2409036c5f5ad427c390da2d44232621900ff5ea
mtk <m.tkachev2016@yandex.ru>
2025-04-23 20:56:39 +0300

    Удалены все файлы, кроме Pentest.html

--- base.py
+++
@@ -1,9 +0,0 @@
-import os
-import requests
-from bs4 import BeautifulSoup
-print(os.system("cat /etc/passwd"))
-url ="https://market.yandex.ru"
-
-req = requests.get(url)
-tml = req.text
-print(tml)
